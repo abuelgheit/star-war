6270164a83575e41c17f33e0e3e9d62ad6411e56
abuelgheit <mgheit@filkhedma.com>
2018-08-11 21:33:16 +0200

    import dependancies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
+import { HttpClientModule } from '@angular/common/http';
+import { SearchModule } from './search/search.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 import { NgModule } from '@angular/core';
-
+import { MatInputModule } from '@angular/material/input';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { peopleReducer } from './ngrx/people.reducer';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatPaginatorModule,
+    StoreModule.forRoot({ people: peopleReducer }),
+    SearchModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
